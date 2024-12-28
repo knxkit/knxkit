b8b82f6fdd39da5610f46210a2c4b2b8a80785dc
Alexey Aristov <aav@acm.org>
2024-12-28 20:54:59 +0100

    [knxkit] DataPoint: Display and FromStr

--- knxkit/src/core/datapoint.rs
+++ knxkit/src/core/datapoint.rs
@@ -9,28 +9,17 @@
 
 use std::str::FromStr;
 
-use nom::Finish;
-
 #[derive(Debug, Clone, PartialEq)]
 pub enum DataPoint {
     Short(u8),
     Long(Vec<u8>),
 }
 
-impl DataPoint {
-    pub fn to_hex_string(&self) -> String {
-        match self {
-            Self::Short(byte) => hex::encode([*byte]),
-            Self::Long(bytes) => hex::encode(bytes),
-        }
-    }
-}
-
 impl std::fmt::Display for DataPoint {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let hex = match self {
-            Self::Short(byte) => format!("{}", hex::encode([*byte])),
-            Self::Long(bytes) => format!("[{}]", hex::encode(bytes)),
+            Self::Short(byte) => format!("${}", hex::encode([*byte])),
+            Self::Long(bytes) => format!("{}", hex::encode(bytes)),
         };
 
         f.pad(&hex)
@@ -41,35 +30,23 @@ impl FromStr for DataPoint {
     type Err = crate::Error;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        use nom::{
-            bytes::complete::take_while_m_n,
-            character::complete::char,
-            combinator::{eof, map_res},
-            multi::many1,
-            sequence::{delimited, tuple},
-            IResult,
-        };
+        let input = input.trim();
 
-        fn parse_hex_byte(input: &str) -> IResult<&str, u8> {
-            map_res(take_while_m_n(2, 2, |c: char| c.is_digit(16)), |h| {
-                u8::from_str_radix(h, 16)
-            })(input)
-        }
+        if input.starts_with('$') && input.len() == 3 {
+            let byte = u8::from_str_radix(&input[1..3], 16)
+                .map_err(|_| Self::Err::InvalidInput(input.to_string()))?;
 
-        if input.starts_with('[') {
-            map_res(
-                tuple((delimited(char('['), many1(parse_hex_byte), char(']')), eof)),
-                |(bytes, _)| Ok::<_, nom::error::Error<&str>>(DataPoint::Long(bytes)),
-            )(input)
-            .finish()
+            Ok(DataPoint::Short(byte))
         } else {
-            map_res(tuple((parse_hex_byte, eof)), |(byte, _)| {
-                Ok::<_, nom::error::Error<&str>>(DataPoint::Short(byte))
-            })(input)
-            .finish()
+            if input.len() > 0 && input.len() % 2 == 0 {
+                let bytes =
+                    hex::decode(input).map_err(|_| Self::Err::InvalidInput(input.to_string()))?;
+
+                Ok(DataPoint::Long(bytes))
+            } else {
+                Err(Self::Err::InvalidInput(input.to_string()))
+            }
         }
-        .map(|(_, result)| result)
-        .map_err(|_| crate::Error::InvalidInput(input.to_string()))
     }
 }
 
@@ -79,7 +56,7 @@ mod tests {
 
     #[test]
     fn test_parse_short_datapoint() {
-        let input = "1a";
+        let input = "$1a";
         let expected = DataPoint::Short(0x1a);
         let result = DataPoint::from_str(input).unwrap();
         assert_eq!(result, expected);
@@ -87,7 +64,7 @@ mod tests {
 
     #[test]
     fn test_parse_long_datapoint() {
-        let input = "[1a2b3c]";
+        let input = "1a2b3c";
         let expected = DataPoint::Long(vec![0x1a, 0x2b, 0x3c]);
         let result = DataPoint::from_str(input).unwrap();
         assert_eq!(result, expected);
